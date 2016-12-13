cfbadd1ee49edc0a0d5ebc9aabc5d33186b08f5c
underwnm <underwnm@gmail.com>
2016-12-12 20:06:13 -0600

    Implement search form with onchange hide.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function changeSearch() {
+    var searchbar = document.getElementById('topSearchForm');
+    var searchdiv = document.getElementById('searchForm');
+    $(searchdiv).hide();
+    $(searchbar).show();
+}
